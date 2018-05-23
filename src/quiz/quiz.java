01b8359385c3145af8b7e9591c78456523db6eec
PDHS lab <pdhslab@dsusd.k12.ca.us>
2018-05-23 11:18:14 -0700

    second commit

--- src/quiz/quiz.java
+++ src/quiz/quiz.java
@@ -2,6 +2,8 @@ package quiz;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.DefaultListModel;
@@ -16,6 +18,10 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
+import GUI_window.ListenForButton;
+import GUI_window.ListenForOpenItem;
+import GUI_window.ListenForText;
+
 
 
 public class quiz {
@@ -23,6 +29,11 @@ public class quiz {
 	JLabel label1;
 	JMenu fileMenu;
 	JMenuItem openMenuItem;
+	JButton buttonA;
+	JPanel panel;
+	JButton buttonB;
+	JButton buttonC;
+	JButton buttonD;
 	
 	
 	public static void main(String[] args) {
@@ -36,6 +47,11 @@ public class quiz {
 		thisGUI.setResizable(false);
 		thisGUI.setLocationRelativeTo(null);
 		thisGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ListenForButtonOK lForButtonOK = new ListenForButtonOK();
+
+		
+		panel = new JPanel();
+		panel.setLayout(null);
 		
 		
 JMenuBar menuBar = new JMenuBar();
@@ -48,8 +64,8 @@ JMenuBar menuBar = new JMenuBar();
 		fileMenu.add(printMenuItem);
 		
 		
-		JMenu editMenu = new JMenu("Edit");
-		JMenuItem copyMenuItem = new JMenuItem("Copy");
+		JMenu editMenu = new JMenu("Pause");
+		JMenuItem copyMenuItem = new JMenuItem("Exit");
 		editMenu.add(copyMenuItem);
 		
 		menuBar.add(fileMenu);
@@ -57,13 +73,59 @@ JMenuBar menuBar = new JMenuBar();
 		thisGUI.add(menuBar);
 		thisGUI.setJMenuBar(menuBar);
 		
+		buttonA  = new JButton("A");
+		buttonA.setBounds(150, 300, 200, 50);
+		buttonA.addActionListener(lForButtonOK);
+		panel.add(buttonA);
+		
+		buttonB = new JButton("B");
+		buttonB.setBounds(450, 300, 200, 50);
+		panel.add(buttonB);
+		
+		buttonC = new JButton("C");
+		buttonC.setBounds(150, 400, 200, 50);
+		panel.add(buttonC);
+		
+		buttonD = new JButton("D");
+		buttonD.setBounds(450, 400, 200, 50);
+		panel.add(buttonD);
+		
+		
+		
+		
+		
+		
+		
+		label1 = new JLabel();
+		label1.setText("Question goes here");
+		label1.setHorizontalAlignment(JLabel.CENTER);
+		label1.setBounds(0,0,800,50);
+		label1.setOpaque(true);
+		label1.setBackground(Color.orange);
+		label1.setFont(new Font(label1.getName(), Font.PLAIN, 18));
+		panel.add(label1);
+		
 		
 		
 
 		
 		
-		
+		thisGUI.add(panel);
 		thisGUI.setVisible(true);
 
+		
+		
 	}
+	private class ListenForButtonOK implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("youve selected answer a");
+		}
+		
+	}
+	
+	
+	
 }
